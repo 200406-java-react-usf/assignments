b0bd5fc5cca6c22deb17f6f40163aa0659b52fad
jbostik <ebiven1563@gmail.com>
2020-04-08 17:57:03 -0400

    Completed Problem 7

---
+++ basic-javascript/Problem7/Problem7.js
@@ -0,0 +1,25 @@
+/**
+ * Write a function that takes in a number and returns the 
+ * corresponding value of the Fibonacci sequence.
+ */
+
+//There is probably a better solution if I learn the math... 
+//but this works for now
+function fibonacciSequence (index) {
+    fibonnaciArray = [0,1]
+    var isNeg = false;
+    if (index < 0) {
+        isNeg = true;
+        index = Math.abs(index);
+    }
+    for (let i = 1; i < index; i++) {
+        fibonnaciArray.push(fibonnaciArray[i] + fibonnaciArray[i-1]);
+    }
+    fibAtIndex = fibonnaciArray[index];
+    if (isNeg) {
+        fibAtIndex = fibAtIndex*-1;
+    }
+    return fibAtIndex;
+}
+
+console.log(fibonacciSequence(10));
