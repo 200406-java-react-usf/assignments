13886c307657e657d0b19bf855d44b44588aeb73
wandeltk <wandeltk@gmail.com>
2020-04-08 17:49:04 -0600

    created a function in order to delete a character at an index

--- basic-javascript/challenge_3/challenge_3.js
+++ basic-javascript/challenge_3/challenge_3.js
@@ -1,1 +1,15 @@
-//challenge_1
+/**Basic JavaScript Challenge 3
+ * 
+ * by Kennedy Wandelt
+ * 
+ * Contains a function which removes the character at a specified index of a string and returns it
+**/
+
+function deleteChar(string, index){
+    str1 = string.slice(0, index)
+    str2 = string.slice((index + 1), (string.length - 1))
+
+    return str1 + str2
+}
+
+//console.log(deleteChar("delete a the 'a'.", 7))
