d011a70e3d267835d11fdffdd43d08a415808a74
wandeltk <wandeltk@gmail.com>
2020-04-08 15:56:27 -0600

    created function to determine if Date parameter is a leap year or not

--- basic-javascript/challenge_1/challenge_1.js
+++ basic-javascript/challenge_1/challenge_1.js
@@ -1,1 +1,24 @@
-//challenge_1
+/**Basic JavaScript Challenge 1
+ * 
+ * by Kennedy Wandelt
+ * 
+ * Contains a function which takes the parameter year and determines if it is a leap year
+**/
+
+
+//cheap trick to account for the local time zone
+
+function isLeapYear(dateToBeChecked){
+    year = dateToBeChecked.getUTCFullYear()
+    if (year % 4 === 0 || year % 400 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+let date = new Date("2000")
+console.log(isLeapYear(date))
+
+
