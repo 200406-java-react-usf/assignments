2b95851b63f486d72a7cab6ddceace935001f4b4
john1eng <john1eng@hotmail.com>
2020-04-08 19:30:18 -0400

    complete assignment 6

---
+++ assignment6/assignment6.js
@@ -0,0 +1,15 @@
+//Write a function to count the number of vowels in a given string.
+
+function countVowel(str){
+    var count = 0;
+    var regex = /a|e|i|o|u/i;
+    for(var i = 0; i<str.length; i++){
+        if(regex.test(str[i]))
+            count++;
+    }
+
+    return count;
+}
+
+//happai = 3 vowels
+console.log(countVowel("happai"));
