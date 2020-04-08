89ca4d738bf3220ae282c7e26709588c67128155
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-08 16:10:13 -0500

    file structure update commit

--- revaboard/problem-3/solution-3.js
+++
@@ -1,6 +0,0 @@
-//take is a string and a position
-function main(input, position){
-    var string = input.split("")
-    var output = string[position];
-    return output;
-}
--- revaboard/problem-4/solution-4.js
+++
@@ -1,5 +0,0 @@
-function main(input){
-    //use replace method
-    var string = input.replace("test", "");
-    return string;
-}
