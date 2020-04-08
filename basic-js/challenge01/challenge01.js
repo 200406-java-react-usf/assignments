28f897fbd579e4573dfa9a7d5a578259f9d06db9
louRemy <louisremy96@gmail.com>
2020-04-08 17:09:50 -0400

    first attempt at the logic for ch1

--- basic-js/challenge01/challenge01.js
+++ basic-js/challenge01/challenge01.js
@@ -1,12 +1,10 @@
-// testing my git flow and making sure I know what I'm doing
 
-//not even sure what challenge 1 is rn
 
-function myfunc(){
-let x = 5;
-return x;
-// heres where some logic would go
+function leapytest(){
+    x = Date.now.prototype.getFullYear()
+    if (x%4 == 0 ){
+        return console.log("It's a leap year!")
+    }
 }
+ leapytest();
 
-
-myfunc();
