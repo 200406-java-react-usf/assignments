e2ffa722cf5ef662536a3d48e4343be6fa7918a2
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-08 15:53:46 -0500

    solution-5 added upper lower case diff

--- revaboard/problem-5/solution-5.js
+++ revaboard/problem-5/solution-5.js
@@ -4,12 +4,23 @@ function main(input, index) {
     console.log(charArray);
     for (i = 0; i <= charArray.length - 1; i++) {
       charArray[i] = charArray[i].charCodeAt(0);
-      //charcode start at 97 ends at 122
-      if (charArray[i]+index > 122){
-        var newCode = charArray[i]+index-26;
-      }else{     
-        var newCode = charArray[i]+index;
-      }
+    //charcode lowercase start at 97 ends at 122
+     if (charArray[i] >= 97 ){
+            if (charArray[i]+index > 122){
+            var newCode = charArray[i]+index-26;
+            }else{     
+            var newCode = charArray[i]+index;
+            }
+        }
+              
+    //charcode uppercase start at 65 ends at 90
+    else{
+            if (charArray[i]+index > 90){
+                var newCode = charArray[i]+index-26;
+                }else{     
+                var newCode = charArray[i]+index;
+                }
+        }
       charArray[i] = String.fromCharCode(newCode);
     }
     return charArray;
---
+++ revaboard/problem-6/solution-6.js
@@ -0,0 +1,4 @@
+function main(input){
+    const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    
+}
