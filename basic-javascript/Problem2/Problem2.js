7b2e56eac36cc6d0d2b83ac209acdcb6c3d1c744
jbostik <ebiven1563@gmail.com>
2020-04-08 16:28:53 -0400

    Added an attempt at problem2; improvements could be made

--- basic-javascript/Problem2/Problem2.js
+++ basic-javascript/Problem2/Problem2.js
@@ -2,6 +2,47 @@
  * Create a function that checks for a valid email format.
  */
 
-var str = "ebiven1563gmail.com";
-var n = str.indexOf("@");
-console.log(n);
+function checkValidEmail (email) {
+    if (hasValidExtension(email) && 
+        hasValidDomain(email) && 
+        hasValidName(email)) {
+        console.log("Email is valid");
+    } else {
+        console.log("Email is invalid")
+    }
+}
+
+//Could potentially turn the valid extensions into a list, and add more
+//invalid extensions
+function hasValidExtension(email) {
+    var valid = false;
+    var extensionIndex = email.lastIndexOf(".");
+    extension = email.slice(extensionIndex);
+    if (extension == '.com' ||
+        extension == '.org' ||
+        extension == '.cc' ||
+        extension == '.co') {
+            valid = true
+        }
+    return valid;
+}
+
+//Could potentially check through a list of invalid characters
+function hasValidDomain(email) {
+    var atIndex = email.indexOf("@")+1;
+    var extensionIndex = email.lastIndexOf(".");
+    var domain = email.substring(atIndex, extensionIndex);
+    if (domain.length > 0) {
+        return true;
+    }
+}
+
+function hasValidName(email) {
+    var atIndex = email.indexOf("@");
+    var name = email.substring(0, atIndex);
+    if (name.length > 0) {
+        return true;
+    }
+}
+
+checkValidEmail('ebiven1563@gmail.cc');
