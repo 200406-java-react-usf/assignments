debc757b2e3036919ef2ccaca5a7d9f7230708e4
john1eng <john1eng@hotmail.com>
2020-04-08 18:42:10 -0400

    commpleted assignment 4

---
+++ assignment4/assignment4.js
@@ -0,0 +1,12 @@
+//Write a function to check if the string "test" is present in a given string. If present, return the string without any occurrences of “test”, otherwise return the original one.
+
+//if the string has test it will return the string without test otherwise return the original string
+
+function replaceWrdTst(str){
+    
+    var regex = /test /gi;
+    var newstr = str.replace(regex, "");
+    return newstr;
+
+}
+console.log("to replace an occurance of test in a string (I love to test a test and make breakfast) is "+ replaceWrdTst("I love to test a test and make breakfast"));
