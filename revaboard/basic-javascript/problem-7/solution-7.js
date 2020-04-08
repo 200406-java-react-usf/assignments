186ee5c22d1c53335eb9697f703fe22dddefc527
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-08 16:48:21 -0500

    commit solution-7 file

---
+++ revaboard/basic-javascript/problem-7/solution-7.js
@@ -0,0 +1,19 @@
+function fibonacci(input){
+    var output = [];
+    output[0] = 0;
+    // per wiki reversed fibonacci starts at 1 as well
+    output[-1] = 1;
+    output[1] = 1;
+    if(input>=2){
+        for (i = 2; i<=input; i++){
+            output[i]=output[i-1]+output[i-2];
+        }
+    }
+    if(input<=0){        
+        for (i = -2; i>=input-1; i--){
+            output[i]=output[i+2]-output[i+1];
+            console.log(i+": "+output[i])
+        }
+    }
+    return output[input];
+}
