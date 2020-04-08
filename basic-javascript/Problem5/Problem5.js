98f49b35900ecd68d35554e5e52ec98443a8789b
jbostik <ebiven1563@gmail.com>
2020-04-08 17:28:06 -0400

    Added a solution to problem 5 (does not currently work for upper case)

---
+++ basic-javascript/Problem5/Problem5.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function that takes in a string and a number as parameters
+ * and implements a Caesar Shift encryption.
+ */
+
+//For the time being, this will only use lower case strings
+function ceasarShift(myString, shift) {
+    myString = myString.toLowerCase();
+    var array = [];
+    for (let i = 0; i < myString.length; i++) {
+        var asciiVal = myString[i].charCodeAt(0);
+        if (asciiVal == 32) {
+            array.push(" ");
+            continue;
+        }
+        asciiVal -= 97;
+        asciiVal = asciiVal - shift;
+        if (asciiVal < 0) {
+            asciiVal += 26;
+        }
+        array.push(String.fromCharCode(asciiVal+97));
+    }
+    console.log(array.join(""));
+}
+
+ceasarShift("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",3);
