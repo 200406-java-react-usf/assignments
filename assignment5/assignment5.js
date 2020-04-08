b6f259bb4b71a33b8dcc3a248fe1425cbcb8cb4d
john1eng <john1eng@hotmail.com>
2020-04-08 19:16:11 -0400

    completed assignment 5

---
+++ assignment5/assignment5.js
@@ -0,0 +1,22 @@
+//Write a function that takes in a string and a number as parameters and implements a Caesar Shift encryption.
+
+function caesarShift(num, str){
+    var result = '';
+    var charCode = 0;
+
+    for(var i=0;i<str.length;i++){
+        //to ignore symboles
+        const regex = /\W/;
+        if(!regex.test(str[i])){
+            charCode = (str[i].charCodeAt()) + num;
+            result += String.fromCharCode(charCode);
+        }    
+        else{
+            result += str[i];
+        }
+    }
+    
+    return result;
+}
+//cde%cde
+console.log(caesarShift(2,'abc%abc'));
