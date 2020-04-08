f844aaaa77ad31f547a1b95e08f01270eea103a8
khkeipe <kkeipe@gmail.com>
2020-04-08 17:54:36 -0500

    header comment added

---
+++ basic-javascript/problem-01/solution-1.js
@@ -0,0 +1,9 @@
+/*
+    Korey Keipe
+    Problem #1
+
+    1.) Create a function that takes a Date object as a parameter and returns true if the year of the provided date is a leap year in the Gregorian calendar.
+*/
+
+
+
