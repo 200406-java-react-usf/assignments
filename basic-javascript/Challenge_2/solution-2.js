a4cd74706b6fcdd34cdbf8f0b592860fff50c483
kosema047 <kosema047@gmail.com>
2020-04-08 20:13:05 -0700

    working on solution 2

---
+++ basic-javascript/Challenge_2/solution-2.js
@@ -0,0 +1,15 @@
+
+//found on w3
+// var email = prompt("Input Email");
+// let val = email.ValidateEmail();
+// function ValidateEmail(InputText){
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     if(InputText.value.match(mailformat)){
+//         document.form1.text1.focus();
+//         return true;
+//     }else{
+//         alert("You have entered an invalid email address!");
+//         document.form1.text1.focus();
+//         return false;
+//     }
+// }
