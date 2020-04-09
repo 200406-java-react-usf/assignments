5156bc3067942ab65610c178da528b91d36f3a99
louRemy <louisremy96@gmail.com>
2020-04-09 14:29:11 -0400

    added more logic to ch01 to check if year is divisible by 100

--- basic-js/challenge01/challenge01.js
+++ basic-js/challenge01/challenge01.js
@@ -3,8 +3,12 @@
 function leapytest(){
     x = Date.now.prototype.getFullYear()
     if (x%4 == 0 ){
-        return console.log("It's a leap year!")
+        console.log("It's a leap year!")
     }
+    else if ( x%100 == 0 && x%400 !== 0){
+        console.log("it's not a leap year")
+    }
+
 }
  leapytest();
 
