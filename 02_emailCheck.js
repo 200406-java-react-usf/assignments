c645b073b92744b0edbbb0d2f58663ef77d42515
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-08 19:28:27 -0500

    remove dup file

--- 02_emailCheck.js
+++
@@ -1,8 +0,0 @@
-//input will be email
-function main(input){
-        //common expression?
-        var output = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
-        console.log(output);
-        return output;
-};
-main();
