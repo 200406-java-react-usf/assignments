a9068bc8f41362b7e9a8fb52eda7778c4bf60001
wandeltk <wandeltk@gmail.com>
2020-04-08 18:50:32 -0600

    Created a function which removes the word test from a string

--- basic-javascript/challenge_4/challenge_4.js
+++ basic-javascript/challenge_4/challenge_4.js
@@ -1,1 +1,24 @@
-//challenge_1
+/**Basic JavaScript Challenge 2
+* 
+* by Kennedy Wandelt
+* 
+* Contains a function which checks for instances of the word test and returns it with those removed 
+**/
+
+function RemoveTest(userString){
+    if (userString.indexOf('test') == -1) {
+        return userString
+    }
+    else {
+        while (userString.indexOf('test') != -1) {
+            strLength = userString.length
+            testIndex = userString.indexOf('test')
+            substr1 = userString.substr(0, testIndex)
+            substr2 = userString.slice(testIndex + 4, strLength)
+            userString = substr1 + substr2
+        }
+        return userString
+    }
+}
+
+//console.log(RemoveTest("testThis is the test string that I want to test"))
