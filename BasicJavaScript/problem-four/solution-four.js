9346915c30cf799661a2ad8a24f24325320167b5
Wagenheim <wagenheimk@me.com>
2020-04-08 22:38:28 -0400

    second attempt at solution four

--- BasicJavaScript/problem-four/solution-four.js
+++ BasicJavaScript/problem-four/solution-four.js
@@ -2,35 +2,34 @@
 var testString = "test";
 
 //string we are searching through to find test
-var stringToFindTestIn = "testing";
+var stringToFindTestIn = "I'm not testing";
 
 //loop to find "test" within the stringToFindTestIn
 
-   for (var i = 0; i < 7; i++){
+   for (var i = 0; i < stringToFindTestIn.length; i++){
 
-    if (stringToFindTestIn[i] == testString[0]){
+        if (stringToFindTestIn[i] == testString[0]){
 
-        if (stringToFindTestIn[i+1] == testString[1]){
+            if (stringToFindTestIn[i+1] == testString[1]){
 
-            if (stringToFindTestIn[i+2] == testString[2]){
+                if (stringToFindTestIn[i+2] == testString[2]){
 
-                if (stringToFindTestIn[i+3] == testString[3]){
+                    if (stringToFindTestIn[i+3] == testString[3]){
 
-                    var partOne = stringToFindTestIn.slice(0, i);
+                        var partOne = stringToFindTestIn.slice(0, i);
 
-                    var partTwo = stringToFindTestIn.slice(i+testString.length, stringToFindTestIn.length);
+                        var partTwo = stringToFindTestIn.slice(i+testString.length, stringToFindTestIn.length);
 
-                    console.log(partOne + partTwo);
+                        console.log(partOne + partTwo);
+
+                    } 
 
                 }
 
             }
 
-        }
-
-    }
+        } 
     
-
    }
 
 
