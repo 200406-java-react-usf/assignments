b8ced6103554f33954a6eec488a64d8383747a62
khkeipe <kkeipe@gmail.com>
2020-04-08 20:27:26 -0500

    working leap year check

--- basic-javascript/problem-01/solution-1.js
+++ basic-javascript/problem-01/solution-1.js
@@ -5,5 +5,18 @@
     1.) Create a function that takes a Date object as a parameter and returns true if the year of the provided date is a leap year in the Gregorian calendar.
 */
 
+var year = new Date();
 
+function leapCheck(Date){
 
+    var fullYear = Date.getFullYear();
+
+        if (fullYear % 4 == 0 || (fullYear % 100 != 0 && fullYear % 400 == 0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+}
+
+console.log(leapCheck(year));
