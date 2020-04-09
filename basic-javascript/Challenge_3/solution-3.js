28de47c198185a48e885a1f6792a8027e4ca9c10
kosema047 <kosema047@gmail.com>
2020-04-08 20:36:32 -0700

    just started working on solution 3, think slice replace

---
+++ basic-javascript/Challenge_3/solution-3.js
@@ -0,0 +1,9 @@
+// var text = new String ("Testing");
+// var pos = str.slice(1);
+
+// function splice(pos){
+//     var newString = input.replace(charAt(index));
+//     return newString;
+// }
+
+// console.log(characterSwap(inputString,index));
