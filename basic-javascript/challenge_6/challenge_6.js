e34d1f293ffae463456dac0520b193f3c1724246
wandeltk <wandeltk@gmail.com>
2020-04-08 22:59:33 -0600

    created a function which counts number of vowels in a string

--- basic-javascript/challenge_6/challenge_6.js
+++ basic-javascript/challenge_6/challenge_6.js
@@ -1,1 +1,22 @@
-//challenge_1
+/**Basic JavaScript Challenge 6
+* 
+* by Kennedy Wandelt
+* 
+* Contains a function which accepts a string parameter and encrypts it via Caesar Shift
+* Assumptions - y is not a vowel, only checking for a,e,i,o,and u
+**/
+
+function countVowels(userString) {
+    var i
+    var numOfVowels = 0
+    userString.toLowerCase()
+    for (i = 0; i < userString.length; i++) {
+        var char = userString.charAt(i)
+        if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+            numOfVowels++
+        }
+    }
+    return numOfVowels
+}
+
+console.log(countVowels('look at this string and gimme vowels'))
