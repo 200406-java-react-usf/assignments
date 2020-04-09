ddc4f03bc287fbe8dcccb09c2d218e90e26e069b
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 00:57:18 -0400

    commented a few lines of code out and added json file

--- basic-javascript/Problem 2/index.js
+++ basic-javascript/Problem 2/index.js
@@ -1,32 +1,35 @@
-const TrialEmail = 'jdvalencia1994@hotmail.com.co';
-if(TrialEmail.indexOf('@') == -1 || TrialEmail.indexOf('.') == -1){
+const TrialEmail = 'jdvalencia_1994@hotmail.com';
+if (TrialEmail.indexOf('@') == -1 || TrialEmail.indexOf('.') == -1) {
     console.log("You have an invalid email");
 }
-const splitEmail = TrialEmail.split('@')
-console.log(splitEmail);
-const prefixEmail = splitEmail[0];
-const suffixEmail = splitEmail[1];
+else {
+    const splitEmail = TrialEmail.split('@')
+    //console.log(splitEmail);
+    const prefixEmail = splitEmail[0];
+    const suffixEmail = splitEmail[1];
 
-function checkprefix(prefix) {
-/*Regex first checks for any word more than 1 occurence then (check for 0 or 1 occurrence of underscore period or dash then checks any new word more than one occurence) 
-then check for 0 or 1 more ocurrence of the parenthesis
-*/
-if(/^\w+([_.-]{0,1}\w+)*$/.test(prefix)){    
-        console.log("Valid username");
+    function checkprefix(prefix) {
+        /*Regex first checks for any word more than 1 occurence then (check for 0 or 1 occurrence of underscore period or dash then checks any new word more than one occurence) 
+        then check for 0 or 1 more ocurrence of the parenthesis
+        */
+        if (/^\w+([_.-]{0,1}\w+)*$/.test(prefix)) {
+            console.log("Valid username");
+        }
+        else {
+            console.log("invalid username");
+        }
     }
-    else{
-        console.log("invalid username");
-    }
-}
 
-function checksuffix(suffix) {
-    //Regex check
-    if(/^\w+(\.?\w+)*$/.test(suffix)){ 
-        console.log("Valid domain");
+    function checksuffix(suffix) {
+        //Regex check
+        if (/^\w+([.-]{0,1}\w+)*$/.test(suffix)) {
+            console.log("Valid domain");
+        }
+        else {
+            console.log("invalid domain");
+        }
     }
-    else{
-        console.log("invalid domain");
-    }
+    checkprefix(prefixEmail);
+    checksuffix(suffixEmail);
+
 }
-checkprefix(prefixEmail);
-checksuffix(suffixEmail);
