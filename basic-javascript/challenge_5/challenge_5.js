cef9db8f313b74aee92db74386737fcd01377a16
wandeltk <wandeltk@gmail.com>
2020-04-08 22:45:52 -0600

    created a function which encrypts a string using caesar shift

--- basic-javascript/challenge_5/challenge_5.js
+++ basic-javascript/challenge_5/challenge_5.js
@@ -1,1 +1,28 @@
-//challenge_1
+/**Basic JavaScript Challenge 2
+* 
+* by Kennedy Wandelt
+* 
+* Contains a function which accepts a string parameter and encrypts it via Caesar Shift
+**/
+
+function caesarShift(userString) {
+    userString.toLowerCase()
+    var newString = ""
+    for (i = 0; i < userString.length; i++) {
+        char = userString.charCodeAt(i)
+        if (char > 96 && char < 122) {
+            char++
+            newString = newString + String.fromCharCode(char)
+        }
+        else if (char == 122) {
+            char = 97
+            newString = newString + String.fromCharCode(char)
+        }
+        else {
+            newString = newString + String.fromCharCode(char)
+        }
+    }
+    return newString
+}
+
+console.log(caesarShift("let's try this again!"))
