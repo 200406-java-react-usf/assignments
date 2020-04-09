00a3137540e0f92100b2c098f6d51f0b957985a3
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-08 20:13:27 -0500

    change to simple logic

--- revaboard/basic-javascript/problem-01/solution-1.js
+++ revaboard/basic-javascript/problem-01/solution-1.js
@@ -2,15 +2,8 @@
 function leapYear(input){
 //take the year 
     var year = input.getFullYear();
-    if(year%4==0){
-        if(year%100 == 0){
-            if (year%400 == 0){
-                return true;
-            }
-            return false;
-        }else{
+    if(year%4==0 && year%100 != 0 || year%400 ==0 ){
             return true;
-        }
     }else{
         return false;
     }
