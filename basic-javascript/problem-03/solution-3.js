82ce27b3d7ef80266386b4f53efb89197f9c0063
khkeipe <kkeipe@gmail.com>
2020-04-10 00:16:42 -0500

    adding git ingnore file

--- basic-javascript/problem-03/solution-3.js
+++ basic-javascript/problem-03/solution-3.js
@@ -5,13 +5,13 @@
     3.) Write a function to remove a character at the specified position of a given string and return the new string.
 */
 
-var inputString = new String('testing');
-var index = 0;
+let inputString = new String('testing');
+let index = 0;
 
 
 function characterSwap(input,postion){
 
-    var newString = input.replace(input.charAt(postion),"");
+    const newString = input.replace(input.charAt(postion),"");
 
     return newString;
 
