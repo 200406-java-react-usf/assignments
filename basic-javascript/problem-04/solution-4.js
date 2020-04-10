f467dd09817e65c1b76d65e92587274aaccc9973
khkeipe <kkeipe@gmail.com>
2020-04-09 20:12:49 -0500

    solution to problem #4

--- basic-javascript/problem-04/solution-4.js
+++ basic-javascript/problem-04/solution-4.js
@@ -6,3 +6,16 @@
 
     4.) Write a function to check if the string "test" is present in a given string. If present, return the string without any occurrences of “test”, otherwise return the original one.
 */
+
+
+function stringTest(input,keyword){
+
+    const newString = input.replace(keyword,"");
+
+    return newString;
+
+}
+
+console.log(stringTest('this is a test string','test'));
+
+
