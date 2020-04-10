6cca065d779531bc90faf5fdd28053f9947581fb
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 21:30:25 -0400

    Changed parameters

--- basic-javascript/Problem 3/index.js
+++ basic-javascript/Problem 3/index.js
@@ -1,6 +1,6 @@
 const charRemoval = (stringObj, indexLocation) => {
-stringObj = new String('Start');
-indexLocation = 3    
+// stringObj = new String('Start');
+// indexLocation = 3    
 if(indexLocation <= stringObj.length){
     let removedChar = stringObj.slice(0,(indexLocation))
     let removedChar2 = stringObj.slice((indexLocation + 1), stringObj.length);
@@ -12,4 +12,4 @@ else{
 }
 }
 
-charRemoval();
+charRemoval('start',3);
