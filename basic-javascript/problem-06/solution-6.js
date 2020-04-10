e9231a69eec6ca8cf36c812badd1fa00b7fa6920
khkeipe <kkeipe@gmail.com>
2020-04-10 01:05:51 -0500

    solution to problem number 6 complete

--- basic-javascript/problem-06/solution-6.js
+++ basic-javascript/problem-06/solution-6.js
@@ -6,3 +6,23 @@
 
     6.) Write a function to count the number of vowels in a given string.
 */
+
+
+function countVauls(input){
+
+    let stringCap = input.toUpperCase();
+    let stringArray = stringCap.split('');
+    let count = 0;
+
+    for (x = 0; x<stringArray.length; x++){
+
+        if(stringArray[x] == "A" ||stringArray[x] == "E" || stringArray[x] == "I" || stringArray[x] == "O" || stringArray[x] == "U"){
+            count = count + 1;            
+        }        
+    }
+
+    console.log(count);
+    
+}
+
+countVauls('Korey Keipe');
