b8afa7d8d18f0e4118d85a73981f2f97a804531b
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 21:38:19 -0400

    Added solution as function

--- basic-javascript/Problem 2/index.js
+++ basic-javascript/Problem 2/index.js
@@ -1,35 +1,35 @@
-const TrialEmail = 'jdvalencia_1994@hotmail.com';
-if (TrialEmail.indexOf('@') == -1 || TrialEmail.indexOf('.') == -1) {
-    console.log("You have an invalid email");
-}
-else {
-    const splitEmail = TrialEmail.split('@')
-    //console.log(splitEmail);
-    const prefixEmail = splitEmail[0];
-    const suffixEmail = splitEmail[1];
-
-    function checkprefix(prefix) {
-        /*Regex first checks for any word more than 1 occurence then (check for 0 or 1 occurrence of underscore period or dash then checks any new word more than one occurence) 
-        then check for 0 or 1 more ocurrence of the parenthesis
-        */
-        if (/^\w+([_.-]{0,1}\w+)*$/.test(prefix)) {
-            console.log("Valid username");
-        }
-        else {
-            console.log("invalid username");
-        }
+//const TrialEmail = 'jdvalencia..1994@hotmail.com';
+const emailChecker = (TrialEmail) => {
+    if (TrialEmail.indexOf('@') == -1 || TrialEmail.indexOf('.') == -1) {
+        console.log("You have an invalid email");
     }
+    else {
+        const splitEmail = TrialEmail.split('@')
+        //console.log(splitEmail);
+        const prefixEmail = splitEmail[0];
+        const suffixEmail = splitEmail[1];
 
-    function checksuffix(suffix) {
-        //Regex check
-        if (/^\w+([.-]{0,1}\w+)*$/.test(suffix)) {
-            console.log("Valid domain");
+        function checkEmailSections(prefix, suffix) {
+            /*Regex first checks for any word more than 1 occurence then (check for 0 or 1 occurrence of underscore period or dash then checks any new word more than one occurence) 
+            then check for 0 or 1 more ocurrence of the parenthesis
+            */
+            if (/^\w+([_.-]{0,1}\w+)*$/.test(prefix)) {
+                if (/^\w+([.-]{0,1}\w+)*$/.test(suffix)) {
+                    console.log("Valid Email!");
+                }
+                else {
+                    console.log("invalid email(domain)");
+                }
+                //console.log("Valid username");
+            }
+            else {
+                console.log("invalid email(username)");
+            }
         }
-        else {
-            console.log("invalid domain");
-        }
+        checkEmailSections(prefixEmail, suffixEmail);
+
+
     }
-    checkprefix(prefixEmail);
-    checksuffix(suffixEmail);
-
 }
+
+emailChecker('jdvalencia_1994@hotmail.com');
