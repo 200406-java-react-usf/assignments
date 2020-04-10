d02d523469e6b50b2540d92c29cf826742067c25
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 21:22:42 -0400

    Changed the code into a function

--- basic-javascript/Problem 3/index.js
+++ basic-javascript/Problem 3/index.js
@@ -1,5 +1,6 @@
-let stringObj = new String('Problematic');
-let indexLocation = 3
+const charRemoval = (stringObj, indexLocation) => {
+stringObj = new String('Start');
+indexLocation = 3    
 if(indexLocation <= stringObj.length){
     let removedChar = stringObj.slice(0,(indexLocation))
     let removedChar2 = stringObj.slice((indexLocation + 1), stringObj.length);
@@ -9,3 +10,6 @@ if(indexLocation <= stringObj.length){
 else{
     console.log("Number out of range");
 }
+}
+
+charRemoval();
