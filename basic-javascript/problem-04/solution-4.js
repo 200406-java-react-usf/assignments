3390cbe916bfc54c38b6de0c2f2152c182524fb8
khkeipe <kkeipe@gmail.com>
2020-04-09 19:47:20 -0500

    header comments added

---
+++ basic-javascript/problem-04/solution-4.js
@@ -0,0 +1,8 @@
+/*
+    Basic JavaScript Assignment
+    Problem #04
+
+    Korey Keipe
+
+    4.) Write a function to check if the string "test" is present in a given string. If present, return the string without any occurrences of “test”, otherwise return the original one.
+*/
