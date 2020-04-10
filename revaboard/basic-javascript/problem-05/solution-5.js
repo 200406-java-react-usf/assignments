0708aa63f0a9b2a7ba8f2c56ede0d9d0d7297b43
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-10 08:55:50 -0500

    solution 5 excluse special char

--- revaboard/basic-javascript/problem-05/solution-5.js
+++ revaboard/basic-javascript/problem-05/solution-5.js
@@ -6,21 +6,21 @@ function main(input, index) {
       charArray[i] = charArray[i].charCodeAt();
       let newCode;
     //charcode lowercase start at 97 ends at 122
-     if (charArray[i] >= 97 ){
+     if (charArray[i] >= 97 && charArray[i] <= 122 ){
             if (charArray[i]+index > 122){
              newCode = charArray[i]+index-26;
             }else{     
              newCode = charArray[i]+index;
             }
-        }
-              
     //charcode uppercase start at 65 ends at 90
-    else{
+        }else if(charArray[i] >= 65 && charArray[i] <= 90){
             if (charArray[i]+index > 90){
                 newCode = charArray[i]+index-26;
                 }else{     
                 newCode = charArray[i]+index;
                 }
+        }else{
+          newCode = charArray[i];
         }
       charArray[i] = String.fromCharCode(newCode);
     }
