834b61fd404042f4fea222f4edefe0c5c4f02f33
wandeltk <wandeltk@gmail.com>
2020-04-09 21:58:11 -0600

    init a function which finds the longest string in an array

--- basic-javascript/challenge_9/challenge_9.js
+++ basic-javascript/challenge_9/challenge_9.js
@@ -1,1 +1,21 @@
-//challenge_1
+/**Basic JavaScript Challenge 9
+* 
+* by Kennedy Wandelt
+* 
+* a function to find the longest string from a given array of strings and returns the string’s array index.
+**/
+
+longestString = (userArray) => {
+    let largestStr = '';
+    largeIndx = null;
+    for (i = 0; i < userArray.length; i++) {
+        x = userArray[i];
+        if (x.length > largestStr.length) {
+            largestStr = x;
+            largeIndx = i;
+        }
+    }
+    return largeIndx;
+}
+
+console.log(longestString(['here', 'is', 'the', 'array']))
