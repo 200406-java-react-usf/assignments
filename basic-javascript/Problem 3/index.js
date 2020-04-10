237551547e62ee6f2967e72a55df803215432049
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 21:12:18 -0400

    Solved challenge 3 using two slice properties and concatenating the remainder strings

---
+++ basic-javascript/Problem 3/index.js
@@ -0,0 +1,11 @@
+let stringObj = new String('Problematic');
+let indexLocation = 3
+if(indexLocation <= stringObj.length){
+    let removedChar = stringObj.slice(0,(indexLocation))
+    let removedChar2 = stringObj.slice((indexLocation + 1), stringObj.length);
+
+    console.log(removedChar + removedChar2)
+}
+else{
+    console.log("Number out of range");
+}
