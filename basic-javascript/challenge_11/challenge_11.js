f0eedd2277dd1f173c3b1d3c309daf9ae3b80774
wandeltk <wandeltk@gmail.com>
2020-04-09 22:28:49 -0600

    init a function which compares arrays for same contents and size

--- basic-javascript/challenge_11/challenge_11.js
+++ basic-javascript/challenge_11/challenge_11.js
@@ -1,1 +1,27 @@
-//challenge_1
+/**Basic JavaScript Challenge 11
+* 
+* by Kennedy Wandelt
+* 
+* A function which checks if two arrays have the same size and contents (including if content order is not the same).
+**/
+
+compareArrays = (array1, array2) => {
+    if (array1.length != array2.length) {
+        return false;
+    }
+    else {
+        array1 = array1.sort();
+        array2 = array2.sort();
+        for (i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i]) {
+                return false;
+            }
+            else{
+                continue;
+            } 
+        }
+    }
+    return true;
+}
+
+console.log(compareArrays([1,2,3,4,5], [5,4,3,2]));
