dd837ab0330ca88b782f50a1389538e426f767d6
khkeipe <kkeipe@gmail.com>
2020-04-10 00:11:08 -0500

    finally solved problem 5

---
+++ basic-javascript/problem-05/solution-5.js
@@ -0,0 +1,40 @@
+/*
+    Basic JavaScript Assignment
+    Problem #05
+
+    Korey Keipe
+
+    5.) Write a function that takes in a string and a number as parameters and implements a Caesar Shift encryption.
+*/
+
+const alpha = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+const alphaA = alpha.split("");
+let alphaShift = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+let newAlpha = alphaShift.split("");
+
+function caesarShift(input,shift){
+
+    for (x = 1; x < shift; x++){
+        shifted = newAlpha.shift();
+        newAlpha.push(shifted)
+    }
+
+    inputArray = input.toUpperCase().split('');
+
+    let output = [];
+
+    for (y = 0; y < inputArray.length; y++){
+        for (z = 0; z < alphaA.length; z++){    
+            if (alphaA[z]==inputArray[y]){
+                output.push(newAlpha[z]);
+            }
+        }        
+    }
+       
+    console.log(output);
+            
+}
+caesarShift('testing',2);
+
+// TESTING shoulld equal --> UFTUJOH
+
