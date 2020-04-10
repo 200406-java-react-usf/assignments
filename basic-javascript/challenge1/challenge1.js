77fcc4143d969c4ac8e4ff81e5c8b7fc8508b4f7
neftaly polanco <polanconeftaly@gmail.com>
2020-04-09 20:46:43 -0400

    commiting solution to challenge1

--- basic-javascript/challenge1/challenge1.js
+++ basic-javascript/challenge1/challenge1.js
@@ -1,2 +1,22 @@
+/*Create a function that takes a Date object
+ as a parameter and returns true if the year of
+ the provided date is a leap year in the Gregorian calendar. */
 
-console.log("hello!")
+
+   
+
+function leapYear (myDate) {
+    //hardcoding the date to test functionality
+    myDate = new Date('01/01/1700');
+    //creating a boolean to check if it is a leap year
+    let isLeap = false;       
+    //obtaining the year from the provided date   
+    let myYear = myDate.getUTCFullYear();
+    //checking if the year provided is a leap year 
+    if (myYear % 400 == 0) isLeap = true;
+    else if (myYear % 100 != 0 && myYear % 4 == 0) isLeap = true;
+    //printing results to console log
+    console.log(isLeap);
+         
+}
+leapYear();
