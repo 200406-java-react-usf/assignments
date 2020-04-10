a61031cb9852f5664527f501992063400d66fd87
neftaly polanco <polanconeftaly@gmail.com>
2020-04-09 21:49:10 -0400

    committing solution to challenge2

--- basic-javascript/challenge2/challenge2.js
+++ basic-javascript/challenge2/challenge2.js
@@ -1,1 +1,8 @@
+//Create a function that checks for a valid email format
 
+function myValidEmail(email) {
+   const emailRegexp = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+   if (emailRegexp.test(email)) console.log(email);
+   else console.log('You provided an invalid Email');
+}
+myValidEmail('email@email.edu');
