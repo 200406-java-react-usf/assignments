cea6bfc2b1fde60f9e410e026a9e62d9a41baaea
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-09 19:54:48 -0700

    update challenge 4

--- basic_javascript/problem_4/challenge4.js
+++ basic_javascript/problem_4/challenge4.js
@@ -1,17 +1,8 @@
 function testSearch(input) {
     input = input.toLowerCase();
-    if (input.includes("test")) {
-        let test = input.toLowerCase();
-
-        return test.replace(/\b(test)\b/gi, '')
-
-    }
-
+    return test.replace(/\b(test)\b/gi, '')
 }
 
 let test = "you are the best test Test ez as pie";
-
-// test = test.replace(/\b(test)\b/gi, '')
-// console.log(test)
 
 console.log(testSearch(test));
