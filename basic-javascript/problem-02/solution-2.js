a9ad325ea20b6f96da9185695c476034fde4614b
khkeipe <kkeipe@gmail.com>
2020-04-09 19:41:34 -0500

    solution 2 solved

--- basic-javascript/problem-02/solution-2.js
+++ basic-javascript/problem-02/solution-2.js
@@ -6,3 +6,18 @@
 
     2.) Create a function that checks for a valid email format.
 */
+
+function emailVerification(email){
+
+    const format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (format.test(email)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let inputEmail = 'kkeipe@gmail.com'
+console.log(emailVerification(inputEmail));
