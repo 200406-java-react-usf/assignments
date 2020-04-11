ec0bc27a29464faed9397e8a8ba770f10a27c971
khkeipe <kkeipe@gmail.com>
2020-04-11 01:47:29 -0500

    solution 9 has been solved

--- my_code/basic-javascript/problem-09/solution-9.js
+++ my_code/basic-javascript/problem-09/solution-9.js
@@ -4,5 +4,18 @@
 
     Korey Keipe
 
-    9.) 
+    9.) Write a function to find the longest string from a given array of strings and returns the string’s array index.
 */
+
+function findStringIndex(stringArray){   
+    
+    let word = '';
+    for (x=0; x<stringArray.length ;x++){
+        if(stringArray[x].length > word.length){
+            word=stringArray[x];
+        }      
+    }
+    index = stringArray.indexOf(word)
+    console.log(index);
+}
+findStringIndex(['this','is','a','test','array','of','strings']);
