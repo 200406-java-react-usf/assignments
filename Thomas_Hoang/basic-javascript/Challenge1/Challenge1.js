2508fad249453872c26536e4938a266559c24904
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-11 15:05:57 -0400

    Added Jest

--- Thomas_Hoang/basic-javascript/Challenge1/Challenge1.js
+++ Thomas_Hoang/basic-javascript/Challenge1/Challenge1.js
@@ -1,3 +1,6 @@
-function CheckLeapYear(Date){
-    
+//Checks if a year is a leap year
+function CheckLeapYear(year){
+    if ((year%4) !== 0) throw Error('This year is not a leap year')
+    else if ((year%4) !== 0)
+    console.log('This is a leap year')
 }
