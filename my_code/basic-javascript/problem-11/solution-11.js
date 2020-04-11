e90fe358ff2a3c071c0c044e004524ec65128c18
khkeipe <kkeipe@gmail.com>
2020-04-11 10:40:23 -0500

    solution to problem 11

--- my_code/basic-javascript/problem-11/solution-11.js
+++ my_code/basic-javascript/problem-11/solution-11.js
@@ -9,3 +9,20 @@
         equivArr([4,2,8,4,7],[7,5,4,2,8]) = false
         equivArr([4,2,8,4],[7,5,4,2,8]) = false
 */
+
+function verifyArrayEquiv(Array1,Array2){
+
+    let sort1 = Array1.sort();
+    let sort2 = Array2.sort();
+    let longArray = Math.max(Array1.length,Array2.length)
+
+    for (x=0;x<longArray;x++){
+        if (Array1[x]!=Array2[x]){
+            return false;
+            break; 
+        }              
+    }
+    return true;   
+}
+
+console.log(verifyArrayEquiv([4,2,8,4,7],[7,4,4,2,8]));
