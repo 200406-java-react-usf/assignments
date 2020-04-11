05d0e94b9e555a05a507d00ed766089ee00d088a
john1eng <john1eng@hotmail.com>
2020-04-11 11:41:43 -0400

    changed stucture of folders and files name

---
+++ Problem-8/solution-8.js
@@ -0,0 +1,4 @@
+// Write a function that adds the values of arrays, even if the arrays are different sizes. 
+
+// 	addArr([1,2,3], [3,2,1]) = [4,4,4]
+// 	addArr([1,2,3,4,5], [1,2,3]) = [2,4,6,4,5]
