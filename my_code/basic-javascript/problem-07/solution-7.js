a89bf53895cccb6ac788f0d3a5208cac1ed92d3e
khkeipe <kkeipe@gmail.com>
2020-04-11 00:04:35 -0500

    problem 7 solved!

--- my_code/basic-javascript/problem-07/solution-7.js
+++ my_code/basic-javascript/problem-07/solution-7.js
@@ -6,3 +6,28 @@
 
     7.) Write a function that takes in a number and returns the corresponding value of the Fibonacci sequence.
 */
+
+// (input-1) = (-1)^(input+1)*
+
+let sequence = [];
+sequence[0] = 0;
+sequence[1] = 1;
+sequence[-1] = 1;
+
+function findFib(input){
+
+if(input>0){
+    for (x = 2; x <= input; x++) {
+        sequence[x] = sequence[x-2] + sequence[x-1];
+    }
+    }else{
+        for (x = -2; x >= input; x--) {
+            sequence[x] = sequence[x+2] - sequence[x+1];
+        }
+    
+    }
+    
+    console.log(sequence[input]);          
+}
+
+findFib(-4);
