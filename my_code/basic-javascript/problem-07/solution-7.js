5972ab5c46a798bdef97259c892132651f85825d
khkeipe <kkeipe@gmail.com>
2020-04-11 00:10:10 -0500

    reformat line spacing

--- my_code/basic-javascript/problem-07/solution-7.js
+++ my_code/basic-javascript/problem-07/solution-7.js
@@ -17,17 +17,13 @@ sequence[-1] = 1;
 function findFib(input){
 
 if(input>0){
-    for (x = 2; x <= input; x++) {
+    for (x = 2; x <= input; x++){
         sequence[x] = sequence[x-2] + sequence[x-1];
     }
-    }else{
-        for (x = -2; x >= input; x--) {
-            sequence[x] = sequence[x+2] - sequence[x+1];
-        }
-    
+}
+    else{
+        for (x = -2; x >= input; x--) sequence[x] = sequence[x+2] - sequence[x+1];
     }
-    
     console.log(sequence[input]);          
 }
-
 findFib(-4);
