fa3e29a4cda3c1b01da3ccb3ce66b375b06993d4
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-10 21:10:01 -0700

    added testing for challenge 3

--- basic_javascript/problem_3/challenge3.js
+++ basic_javascript/problem_3/challenge3.js
@@ -1,8 +1,16 @@
 function removeChar(input, position) {
+    if (typeof(input) != "string") {
+        return ("First parameter needs to be a string")
+    }
     if (input[position] == undefined) {
-        throw ("Gave me wrong inputs try again");
+        return ("Second parameter needs to be a number or number is not in range of string");
     }
     word = input.replace(input[position], '');
     return word
 }
+
 console.log(removeChar("jeremy is very cool", 18));
+
+module.exports = {
+    removeChar
+}
---
+++ basic_javascript/problem_3/challenge3.test.js
@@ -0,0 +1,13 @@
+const sut = require('./challenge3')
+
+test("Removes character in a given spot", () => {
+    expect(sut.removeChar("jeremy is very cool", 18))
+});
+
+test("suppose to return error message", () => {
+    expect(sut.removeChar("jeremy is very cool", 20))
+});
+
+test("suppose to return error message ", () => {
+    expect(sut.removeChar(20, "jeremy is very cool"))
+});
