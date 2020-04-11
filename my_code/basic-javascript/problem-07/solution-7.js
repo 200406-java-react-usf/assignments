b3d5cad383b17a266a2c1e44bf307e7c8891e55a
khkeipe <kkeipe@gmail.com>
2020-04-10 21:57:47 -0500

    comment header added

---
+++ my_code/basic-javascript/problem-07/solution-7.js
@@ -0,0 +1,8 @@
+/*
+    Basic JavaScript Assignment
+    Problem #07
+
+    Korey Keipe
+
+    7.) Write a function that takes in a number and returns the corresponding value of the Fibonacci sequence.
+*/
