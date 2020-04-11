0a1d28681da965929bb5fbb29fe82976427fbb8e
khkeipe <kkeipe@gmail.com>
2020-04-11 10:21:03 -0500

    added header comments

---
+++ my_code/basic-javascript/problem-11/solution-11.js
@@ -0,0 +1,11 @@
+/*
+    Basic JavaScript Assignment
+    Problem #11
+
+    Korey Keipe
+
+    11.) Check if two arrays have the same size and contents (including if content order is not the same).
+        equivArr([4,2,8,4,7],[7,4,4,2,8]) = true
+        equivArr([4,2,8,4,7],[7,5,4,2,8]) = false
+        equivArr([4,2,8,4],[7,5,4,2,8]) = false
+*/
