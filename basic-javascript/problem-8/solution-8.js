a4aedec916a0057c769d15ddebc6c97175b43256
Scotty Thoms <spthoms19@gmail.com>
2020-04-12 16:09:29 -0500

    solution and test for problem-8 complete

--- basic-javascript/problem-8/solution-8.js
+++ basic-javascript/problem-8/solution-8.js
@@ -1,6 +1,6 @@
 const addArr = (array1, array2) => {
 
-    if(typeof array1 !== 'object' || typeof array2 !== 'object') throw Error ('You did not send me valid data.');
+    if(typeof array1 !== typeof [] || typeof array2 !== typeof []) throw Error ('You did not send me valid data.');
     if(array1 === [] && array2 === []) throw Error ('You sent me two empty arrays!');
 
 
