34660076cc8e4a667351e02a4ba8809382d5bb19
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-11 21:26:03 -0400

    Finished Challenge 6, however the counter shows each iteration

--- Thomas_Hoang/basic-javascript/Challenge6/Challenge6.js
+++ Thomas_Hoang/basic-javascript/Challenge6/Challenge6.js
@@ -1,1 +1,20 @@
 //Write a function to count the number of vowels in a given string.
+
+function vowelCounter(str){
+    const vowels = ('AEIOUY')
+    const upperStr = str.toUpperCase()
+    console.log(upperStr.length)
+
+    var vowelCount = 0
+    //Loops through each character in the string and checks if it is a vowel and then counts it
+    for (i = 0; i < upperStr.length; i++){
+        //Uppercase
+        for (q = 0; q < vowels.length; q++){
+            if (vowels.charAt(q) == upperStr.charAt(i)){
+                vowelCount = vowelCount + 1
+                console.log(vowelCount)
+            }
+    }
+}
+}
+vowelCounter("This is my test string")
