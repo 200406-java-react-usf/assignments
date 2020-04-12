bf42d94310f832f3a477303ae05bb2c85c284e49
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-11 22:17:37 -0400

    Challenge 7 nearly complete, however can't get the function to return values.

--- Thomas_Hoang/basic-javascript/Challenge7/Challenge7.js
+++ Thomas_Hoang/basic-javascript/Challenge7/Challenge7.js
@@ -1,1 +1,15 @@
 //Write a function that takes in a number and returns the corresponding value of the Fibonacci sequence.
+
+function fibonnacci(n){
+    let sequence = []
+    sequence[0] = 1
+    sequence[1] = 1
+
+    for(i = 0; i < n + 1; i ++){
+        console.log(i)
+    sequence[i] = sequence[i - 1] + sequence[i - 2]
+    }
+    console.log(sequence[1 - 1])
+}
+
+fibonnacci(1)
--- Thomas_Hoang/basic-javascript/Challenge8/Challenge8.js
+++ Thomas_Hoang/basic-javascript/Challenge8/Challenge8.js
@@ -1,1 +1,4 @@
 //Write a function that adds the values of arrays, even if the arrays are different sizes. 
+array1 = (0 , 1, 2, 3, 4, 5)
+console.log(array1[1])
+//function differentSizeArray()
