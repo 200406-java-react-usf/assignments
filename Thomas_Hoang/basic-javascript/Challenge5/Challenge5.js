8c73d6ec38df1f744116117b8dac0c0f0c4e1030
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-11 20:13:49 -0400

    Finished Challenge 5.

--- Thomas_Hoang/basic-javascript/Challenge5/Challenge5.js
+++ Thomas_Hoang/basic-javascript/Challenge5/Challenge5.js
@@ -1,1 +1,35 @@
 //Write a function that takes in a string and a number as parameters and implements a Caesar Shift encryption.
+
+function caesarShift(str, key){
+    //Creates the inital alphabet and then converts them into a encrypted form based on the given key
+    const alphabet = " ABCDEFGHIJKLMONPQRSTUVWXYZ0123456789"
+    const alphabet2 = alphabet.toLowerCase()
+    const encryptedAlphabet = alphabet.substring(key) + alphabet.substring(0,key)
+    const encryptedAlphabet2 = encryptedAlphabet.toLowerCase()
+
+    //Initializes variables
+    const newStr = str
+    const newChar = ''
+    const currChar = ''
+    let index = 1
+    //Loop to go through the alphabet and set the new character to the encrypted equivalent
+    for (i = 0; i < str.length; i++){
+        currChar = newStr.charAt(i)
+        //Uppercase
+        index = alphabet.indexOf(currChar)
+        if (index !== -1) {
+            newChar = encryptedAlphabet.charAt(index);
+            newStr.setCharAt(i, newChar)
+        }
+        //Lowercase
+        index = alphabet2.indexOf(currChar)
+        if (index != -1) {                
+            encryptedAlphabet2 = encryptedAlphabet2.toLowerCase();                
+            newChar = encryptedAlphabet2.charAt(index);                
+            newStr.setCharAt(i, newChar);
+        }
+    }
+        return newStr.toString()
+}
+
+caesarShift("This is my test encryption", 6)
