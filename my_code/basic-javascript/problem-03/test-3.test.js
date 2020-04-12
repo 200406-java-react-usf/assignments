b3d98679ee08934e14e6e0b4b13e07b89ab511fd
khkeipe <kkeipe@gmail.com>
2020-04-11 21:02:08 -0500

    test 3 updated

--- my_code/basic-javascript/problem-03/test-3.test.js
+++ my_code/basic-javascript/problem-03/test-3.test.js
@@ -1,7 +1,7 @@
 const sut = require('./solution-3');
 
 
-describe ('remove a charachter at a specified index and return new string without that character',() => {
+describe('remove a charachter at a specified index and return new string without that character',() => {
 
 
     test('should return a string \'Tesing', done =>{
