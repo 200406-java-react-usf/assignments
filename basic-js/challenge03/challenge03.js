036e18db5b2ed9161386f5880d83391642b81229
louRemy <louisremy96@gmail.com>
2020-04-12 14:32:17 -0400

    ch 3 mvp done

--- basic-js/challenge03/challenge03.js
+++ basic-js/challenge03/challenge03.js
@@ -1,5 +1,8 @@
 function removeCharacter(givenString, letterToRemove){
-    givenString.replace(letterToRemove, " ")
+    let letterRegex = new RegExp(letterToRemove, "gi")
+    return givenString.replace((letterRegex), " ")
 }
+
+console.log(removeCharacter( "Hello my name is louis and I dont like the letter h", "h"))
 
 
