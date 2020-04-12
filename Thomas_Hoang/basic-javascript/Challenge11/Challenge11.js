13d207f8b62a0db18046e39a30171fb1ff43759a
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-12 15:27:13 -0400

    Completed Challenge 11

--- Thomas_Hoang/basic-javascript/Challenge11/Challenge11.js
+++ Thomas_Hoang/basic-javascript/Challenge11/Challenge11.js
@@ -2,10 +2,21 @@
 arr1 = [1, 2, 3]
 arr2 = [3, 2, 1]
 function checkArrays(array1, array2){
-    const isTrue = true
+    let isTrue = true
+    //Determines if the arrays are the same size
     if (array1.length !== array2.length){
         isTrue = false
     }
-    return(isTrue)
+    for(i = 0; i < array1.length; i++){
+        //Checks 1 number against the others whole array contents
+        isTrue = array1.includes(array2[i])
+
+        if (isTrue === false){
+            return(isTrue)
+        }else if ((i = array1.length) && isTrue === true){
+            return(isTrue)
+            }
+        }
+    console.log(isTrue)
 }
 checkArrays(arr1, arr2)
