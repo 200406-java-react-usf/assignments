a4ed9f82c7f48eb554b0b50ddfd2c336d232f7c0
khkeipe <kkeipe@gmail.com>
2020-04-11 21:01:11 -0500

    test 4 complete

--- my_code/basic-javascript/problem-04/test-4.test.js
+++ my_code/basic-javascript/problem-04/test-4.test.js
@@ -1,1 +1,12 @@
 const sut = require('./solution-4');
+
+describe('check if string \'test\' is within a given string', () =>{
+
+    test('should output a new string with test removed', done =>{
+
+            let string = 'this is a test string'
+            let keyword = 'test'
+            expect(sut(string,keyword)).toBe('this is a string');
+            done();
+    });
+});
