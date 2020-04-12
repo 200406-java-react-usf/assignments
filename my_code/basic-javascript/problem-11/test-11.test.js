a18560d7c6f1e240cd4d78b66f7d082efd8e2fef
khkeipe <kkeipe@gmail.com>
2020-04-11 22:43:22 -0500

    added test for problem 11

--- my_code/basic-javascript/problem-11/test-11.test.js
+++ my_code/basic-javascript/problem-11/test-11.test.js
@@ -1,1 +1,14 @@
 const sut = require('./solution-11');
+
+
+describe('Check if two arrays have the same size and contents', () =>{
+   
+    test('should be true', done =>{
+        expect(sut([4,2,8,4,7],[7,4,4,2,8])).toBe(true);
+        done();
+    });
+    test('should be false', done =>{
+        expect(sut([4,2,10,4,7],[7,4,4,2,8])).toBe(false);
+        done();
+    });
+});
