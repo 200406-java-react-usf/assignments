b04dbb757bc1262bfcfd18606ac7409b151f873f
KaneishaR <kaneisha.reese@gmail.com>
2020-04-12 07:55:21 -0500

    Solution 3 added check for integer

--- basic_js/problem-3/solution-3.js
+++ basic_js/problem-3/solution-3.js
@@ -1,20 +1,30 @@
-function removeChar(string, index, onComplete, onError) {
+function removeChar(str, index, onComplete, onError) {
     //setTimeout(() =>{
-            //validate that the index is in the string
-        this.str = string;
-        this.index = index;
-        length = string.length;
-        if (index > length) {
-            onError('Oh no the index is not in the string');
+        if (typeof str != "string"){
+            onError('input not a string');
+        }
+        else if (!Number.isInteger(index)){
+            onError('index not a integer');
+        }
+        else{
+
+            length = str.length;
+            if (index > length) {
+                onError('Oh no the index is not in the string');
+                return;
+            }
+
+            let returnedString =str.slice(0, index) + str.slice(index+1);
+            onComplete(returnedString);
             return;
         }
-        
-        let returnedString =str.slice(0, index) + str.slice(index+1);
-        onComplete(returnedString);
-        return;
-        
     //}, 250);
 };
 
-//removeChar('test', 6, str => console.log(str), err => console.log(err))
+removeChar('test', 2, str => console.log(str), () => {});
+removeChar('test', 6, () => {}, err => console.log(err));
+removeChar('test', 'a', () => {}, err => console.log(err));
+removeChar(25, 6, () => {}, err => console.log(err));
+
+
 module.exports= {removeChar};
