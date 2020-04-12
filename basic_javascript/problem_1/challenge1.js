f15722d575778001ec37ab7dc426676ce5b7309c
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-11 23:40:49 -0700

    update unit testing

--- basic_javascript/problem_1/challenge1.js
+++ basic_javascript/problem_1/challenge1.js
@@ -1,7 +1,18 @@
 function leapYear(userInput) {
+    if (typeof(userInput) != 'string')
+        return (`Please provide a string with a date format of "mm/dd/yyyy"`)
+    if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(userInput))
+        return (`Please Provide date formate in "mm/dd/yyyy"`);
+
     var s = Date.parse(userInput);
+    console.log(typeof(s));
+
     var d = new Date(s);
+    console.log(typeof(d));
+
     var date = d.getFullYear(s);
+    console.log(typeof(date));
+
 
     return (((date % 4 === 0) && (date % 100 !== 0)) || date % 400 === 0) ? true : false;
 
@@ -10,3 +21,5 @@ function leapYear(userInput) {
 module.exports = {
     leapYear,
 };
+
+console.log(leapYear('11-24-2019'));
--- basic_javascript/problem_1/challenge1.test.js
+++ basic_javascript/problem_1/challenge1.test.js
@@ -1,10 +1,35 @@
 const sut = require('./challenge1');
 
+const testing = ['04/24/1804', '04/24/2020', '04/24/2016', '04/24/2072',
+    '04/24/2012', '04/24/2080'
+]
+const falseTesting = ['04/24/1803', '04/24/2019', '04/24/2015', '04/24/2014',
+    '04/24/2010', '04/24/2081'
+]
 
-test('finds if the given date is a leap year', () => {
-    expect(sut.leapYear('April 20,2020')).toBe(true);
-});
+const inputTesting = [1804, 2020, 2016, 2072, 2012, 2080]
+const formatTesting = ['April 20, 2020', '11-24-2019', '2020-24-11']
 
-test('finds if the given date is a leap year', () => {
-    expect(sut.leapYear('April 20,2019')).toBe(false);
-});
+for (let i = 0; i < testing.length; i++) {
+    test('finds if the given date is a leap year', () => {
+        expect(sut.leapYear(testing[i])).toBe(true);
+    })
+};
+
+for (let i = 0; i < falseTesting.length; i++) {
+    test('Confirms given year is not a leap year', () => {
+        expect(sut.leapYear(falseTesting[i])).toBe(false);
+    })
+};
+
+for (let i = 0; i < falseTesting.length; i++) {
+    test('testing error message', () => {
+        expect(sut.leapYear(inputTesting[i])).toBe(`Please provide a string with a date format of "mm/dd/yyyy"`);
+    })
+};
+
+for (let i = 0; i < formatTesting.length; i++) {
+    test('testing error message', () => {
+        expect(sut.leapYear(formatTesting[i])).toBe(`Please Provide date formate in "mm/dd/yyyy"`);
+    })
+};
