2aefceb1c1bcca1ac3b346ec5ba231322faa5897
neftaly polanco <polanconeftaly@gmail.com>
2020-04-13 16:08:06 -0400

    committing solution to challenge2

--- basic-javascript/challenge2/challenge2.js
+++ basic-javascript/challenge2/challenge2.js
@@ -2,8 +2,8 @@
 
  function myValidEmail(email) {
    const emailRegexp = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-   if (emailRegexp.test(email)) return email;
-   else  return ('You provided an invalid Email');
+   if (emailRegexp.test(email)) return true;
+   else  throw('You provided an invalid Email');
 }
 module.exports = {
    myValidEmail
--- basic-javascript/challenge2/challenge2.test.js
+++ basic-javascript/challenge2/challenge2.test.js
@@ -2,14 +2,13 @@ const sut = require('./challenge2');
 
 describe('challenge_2', () => {
     test('Should return provided email if email is valid', () => {
-        sut.myValidEmail('bigmama@mlawrence.com', () => {
-            expect(sut.email);
-        });
+        expect(sut.myValidEmail('bigmama@mlawrence.com')).toBe(true);
     });
 
     test('Should return You provided an invalid Email if missing @', () => {
-        sut.myValidEmail('bigmamamlawrence.com', () => {
-            expect('You provided an invalid Email');
-        });        
+        expect(() => {
+            sut.myValidEmail('bigmamamlawrence.com');        
+        }).toThrow();
     });
 });
+
