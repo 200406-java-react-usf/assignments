c2153d836940103413e53c6821dd3fa123ae8809
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-13 18:28:26 -0400

    Fixed Challenge 5 jest testing to work.

--- Thomas_Hoang/basic-javascript/Challenge5/Challenge5.test.js
+++ Thomas_Hoang/basic-javascript/Challenge5/Challenge5.test.js
@@ -2,13 +2,11 @@ const sut = require('./Challenge5.js');
 const str = ('This is my test encryption')
 describe('Caesar Shift Test', () => {
     test('Should return same string', done => { 
-        expect( () => {sut(str,0)}).toBe("This is my test encryption");
+        expect(sut(str,0)).toBe('This is my test encryption');
         done();
 })
     test('Should return encrypted string', done => { 
-        expect( () => {
-            sut(str,2)
-        }).toBe('Vjkubkubn0bvguvbgqet0rvkpq');
+        expect(sut(str,2)).toBe('Vjkubkubn0bvguvbgqet0rvkpq');
         done();
 })
 });
