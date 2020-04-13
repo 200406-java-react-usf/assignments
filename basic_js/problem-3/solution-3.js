c74f9f94e5f1151640ed536a19de7390f088f6a2
KaneishaR <kaneisha.reese@gmail.com>
2020-04-13 14:45:09 -0500

    Problem 3 final

--- basic_js/problem-3/solution-3.js
+++ basic_js/problem-3/solution-3.js
@@ -11,20 +11,20 @@ function removeChar(str, index, onComplete, onError) {
             length = str.length;
             if (index > length) {
                 onError('Oh no the index is not in the string');
-                return;
+            
             }
 
             let returnedString =str.slice(0, index) + str.slice(index+1);
             onComplete(returnedString);
-            return;
+          
         }
     //}, 250);
 };
 
-removeChar('test', 2, str => console.log(str), () => {});
-removeChar('test', 6, () => {}, err => console.log(err));
-removeChar('test', 'a', () => {}, err => console.log(err));
-removeChar(25, 6, () => {}, err => console.log(err));
+// removeChar('test', 2, str => console.log(str), () => {});
+// removeChar('test', 6, () => {}, err => console.log(err));
+// removeChar('test', 'a', () => {}, err => console.log(err));
+// removeChar(25, 6, () => {}, err => console.log(err));
 
 
 module.exports= {removeChar};
--- basic_js/problem-3/solution-3.test.js
+++ basic_js/problem-3/solution-3.test.js
@@ -10,8 +10,32 @@ describe('Solution 3 Test', () => {
     test('Should return the string missing the character at the give index', done =>{
         sut.removeChar('Test', 3, str =>{
             expect(str).toBeTruthy();
-            expect(str).toEqual('Thi is a test.')
+            expect(str).toEqual('Tes')
             done();
         }, ()=>{});
-    }, 15000);
-}, )
+    });
+
+    test('Should return error becuase index is out of array', done =>{
+        sut.removeChar('Test', 6, () =>{}, err=> {
+            expect(err).toBeTruthy();
+            expect(err).toEqual('Oh no the index is not in the string')
+            done();
+        });
+    });
+
+    test('Should return error becuase index is not a number', done =>{
+        sut.removeChar('Test', 'a', () =>{}, err=> {
+            expect(err).toBeTruthy();
+            expect(err).toEqual('index not a integer')
+            done();
+        });
+    });
+
+    test('Should return error becuase input not a string', done =>{
+        sut.removeChar(25, 'a', () =>{}, err=> {
+            expect(err).toBeTruthy();
+            expect(err).toEqual('input not a string')
+            done();
+        });
+    });
+});
