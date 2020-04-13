43a355a9c8aae6f3a8e9e9e97cbdd5988c0ad126
Jose Angeles <humblekgames@gmail.com>
2020-04-13 12:07:19 -0400

    Finished Challenge7 with test

--- basic-javascript/Challenge7/challenge7.js
+++ basic-javascript/Challenge7/challenge7.js
@@ -1,15 +1,10 @@
  const fib = (n) => 
  { 
-    if(n===0) return 0;
+    if (n < 0) return -1;
+    if(n=== 0) return 0;
     if(n <= 2) return 1;
-    if (n > 2)
-    {
-        return fib(n-1) + fib(n-2);
-    }
-    else
-    {
-        return Math.pow(-1, (n + 1)) * fib(Math.abs(n));
-    }
+    
+    return fib(n-1) + fib(n-2);
  };
 
  module.exports = 
