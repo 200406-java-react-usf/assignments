df621c893bf7b98d49ccd14635327efdf3ac30d2
neftaly polanco <polanconeftaly@gmail.com>
2020-04-13 16:07:13 -0400

    committing solution and test to challenge1

--- basic-javascript/challenge1/challenge1.js
+++ basic-javascript/challenge1/challenge1.js
@@ -5,22 +5,24 @@
 
    
 
-const leapYear = (myDate, cb) => {
-    if(myDate.constructor.toString().indexOf("Date") <= -1) return 'wrong input format, please provide a date';
+function leapYear(myDate) {
+    if(myDate.constructor.toString().indexOf("Date") <= -1) throw('wrong input format, please provide a date');
     //creating a boolean to check if it is a leap year
     let isLeap = false;       
     //obtaining the year from the provided date   
     let myYear = myDate.getFullYear();
     //checking if the year provided is a leap year 
-    if (myYear % 400 == 0) isLeap = true;
-    else if (myYear % 100 != 0 && myYear % 4 == 0) isLeap = true;
+    if (myYear % 400 == 0) 
+    isLeap = true;
+    else if (myYear % 100 != 0 && myYear % 4 == 0) 
+    isLeap = true;
     //printing results to console log
    // console.log(isLeap);
    //cb(isLeap);
-   return isLeap;
-         
+   return isLeap;         
 }
-console.log(leapYear(new Date('01/01/2000')))
+
+//console.log(leapYear(new Date('01/01/2000')))
 module.exports = {
     leapYear
 }
--- basic-javascript/challenge1/challenge1.test.js
+++ basic-javascript/challenge1/challenge1.test.js
@@ -3,20 +3,16 @@ const sut = require('./challenge1');
 describe('challenge_1', () => {
 
     test('should return leap year == true when year is divisible by 400', () => {
-        sut.leapYear(new Date('01/01/2000'), () => {
-            expect(sut.isLeap).toBeTruthy();                        
-        });        
+        expect(sut.leapYear(new Date('01/01/2000'))).toBeTruthy();                                      
     });
 
     test('Should return false if the year is divisible by 100 but not by 400', () =>{
-        sut.leapYear(new Date('01/01/1999'), () => {
-            expect(sut.isLeap).toBeFalsy();
-        })
+       expect(sut.leapYear(new Date('01/01/1999'))).toBeFalsy();
     })
     
     test('Should return wrong input if input is not date format', () => {
-        sut.leapYear('33665555', () => {
-            expect('wrong input format, please provide a date');
-        })
-    })
+        expect(() => {
+            sut.leapYear('3335555');
+        }).toThrow();
+    });
 });
