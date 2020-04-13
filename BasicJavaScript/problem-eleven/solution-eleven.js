c5ab5fe6d76ffc6da0bd6a89cdfc4aeb56dfc951
Wagenheim <wagenheimk@me.com>
2020-04-12 20:14:07 -0400

    finished problem 11

--- BasicJavaScript/problem-eleven/solution-eleven.js
+++ BasicJavaScript/problem-eleven/solution-eleven.js
@@ -2,37 +2,41 @@
 
     Idea:
         -first check they are the same length
-        -loop through first array input, and use .includes to see if the value is contained in the other
+        -sort them in order, this way duplicates are checked
+        -loop through and make sure each value is the same in each array
+        -return false if one value is not the same 
 
 */
 
-let one = [4,2,8,5,7];
-let two = [7,4,4,2,8];
+// let one = [4,2,8,4,7];
+// let two = [7,4,4,2,8];
 
 let equivArray = (arrayOne, arrayTwo) => {
     
-    if (arrayOne.length !== arrayTwo.length) throw new Error('Arrays are two different sizes');
+    if (arrayOne.length !== arrayTwo.length) return false;
 
-    let boolArray = [];
+    arrayOne = arrayOne.sort();
+
+    arrayTwo = arrayTwo.sort();
+
 
     for (var i = 0; i < arrayOne.length; i++){   
 
-        boolArray.push(arrayTwo.includes(arrayOne[i]));
-        
-    }
-
-    for (var j = 0; j < boolArray.length; j++){
-
-        if (boolArray[j] === false){
+        if (arrayOne[i] !== arrayTwo[i]){
 
             return false;
-            break;
 
         }
-
+        
     }
 
     return true;
 
 }
-console.log(equivArray(one,two));
+//console.log(equivArray(one,two));
+
+module.exports = {
+
+    equivArray
+
+}
---
+++ BasicJavaScript/problem-eleven/test-eleven.test.js
@@ -0,0 +1,36 @@
+const sut = require('./solution-eleven');
+
+
+describe('testing multiple arrays to see if they have the same values, regardless of order', () =>{
+
+    test('testing first given example, should return true', () => {
+
+        expect(sut.equivArray([4,2,8,4,7],[7,4,4,2,8])).toBe(true);
+
+    });
+
+    test('testing second given example, should return false', () => {
+
+        expect(sut.equivArray([4,2,8,4,7],[7,5,4,2,8])).toBe(false);
+
+    });
+
+    test('testing third given example, should return false', () => {
+
+        expect(sut.equivArray([4,2,8,4],[7,5,4,2,8])).toBe(false);
+
+    });
+
+    test('testing my own example, should return true', () => {
+
+        expect(sut.equivArray([1,2,3,4,5,6],[6,3,4,2,5,1])).toBe(true);
+
+    });
+
+    test('testing my own example, should return false', () => {
+
+        expect(sut.equivArray([1,2,3,4,5,6],[7,8,9,10,1,12])).toBe(false);
+
+    });
+
+});
