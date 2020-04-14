e2173aa871d780a7c7f2d19cb2a33e694d2b5ba1
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-13 22:16:33 -0700

    update testing

--- basic_javascript/problem_6/challenge6.test.js
+++ basic_javascript/problem_6/challenge6.test.js
@@ -1,15 +1,15 @@
 const sut = require('./challenge6');
 
 test("Will output correct information when it is given a string", () => {
-    expect(sut.getVowels(`going to test how this works`))
+    expect(sut.getVowels(`going to test how this works`)).toBe(7)
 })
 
 test("Will throw error message because a string wasn't given", () => {
-    expect(sut.getVowels(7))
+    expect(sut.getVowels(7)).toBe("Please give me a string value")
 })
 test("Will throw error message because a string wasn't given", () => {
-    expect(sut.getVowels(null))
+    expect(sut.getVowels(null)).toBe("Please give me a string value")
 })
 test("Will throw error message because a string wasn't given", () => {
-    expect(sut.getVowels('object'))
+    expect(sut.getVowels('object')).toBe(2)
 })
