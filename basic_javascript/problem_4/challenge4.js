865c62f7960f9b9134d3ba18781a5fa4d0908579
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-13 21:46:11 -0700

    changed the algorithm

--- basic_javascript/problem_4/challenge4.js
+++ basic_javascript/problem_4/challenge4.js
@@ -2,8 +2,7 @@ function testSearch(input) {
     if (typeof(input) != 'string')
         return "Input incorrect. Please put in a string"
 
-    input = input.toLowerCase();
-    return input.replace(/\b(test)\b/gi, '')
+    return input.replace(/test/g, '');
 }
 module.exports = {
     testSearch
