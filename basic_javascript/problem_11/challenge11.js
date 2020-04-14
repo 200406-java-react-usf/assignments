db51edfc7339ed66c259d4ebf248e5c9c5da92a9
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-13 22:14:34 -0700

    removed console.log()

--- basic_javascript/problem_11/challenge11.js
+++ basic_javascript/problem_11/challenge11.js
@@ -1,6 +1,4 @@
 function compareArray(arr1, arr2) {
-    console.log(arr1);
-    console.log(typeof(arr2));
 
     if (typeof(arr1) == 'undefined' || typeof(arr2) == 'undefined') { return "Please input an array" };
     if (typeof(arr1) != 'object' || typeof(arr2) != 'object') { return "Please input an array" };
@@ -15,5 +13,3 @@ function compareArray(arr1, arr2) {
 module.exports = {
     compareArray
 }
-
-console.log(compareArray([], [6, 1, 9, 1, 2, 7, 7, 2, 7]));
