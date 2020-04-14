7c8a83e806d697d8ebc0bb59d111c63bda4fd411
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-13 21:43:11 -0700

    minor updates

--- basic_javascript/problem_3/challenge3.js
+++ basic_javascript/problem_3/challenge3.js
@@ -1,15 +1,13 @@
 function removeChar(input, position) {
     if (typeof(input) != "string") {
-        return ("First parameter needs to be a string")
+        throw Error("First parameter needs to be a string")
     }
-    if (input[position] == undefined) {
-        return ("Second parameter needs to be a number or number is not in range of string");
+    if (typeof(position) != 'number') {
+        throw Error("Second parameter needs to be a number or number is not in range of string");
     }
     word = input.replace(input[position], '');
     return word
 }
-
-console.log(removeChar("jeremy is very cool", 18));
 
 module.exports = {
     removeChar
