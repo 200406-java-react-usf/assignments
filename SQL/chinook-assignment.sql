e331a85a47dd294fdaa0808d325a6679834943f8
Wagenheim <wagenheimk@me.com>
2020-05-07 01:53:50 -0400

    pushing where im at on sql workbook

--- SQL/chinook-assignment.sql
+++ SQL/chinook-assignment.sql
@@ -199,6 +199,96 @@ call namesOfEmployees();
 
 
 
+/*
+ * 
+ * 6.0 
+ * 
+ */
+
+
+--6.1
+
+--Task 1
+
+/*drop function select_all_employees() 
+
+create or replace function select_all_employees()
+returns trigger as $$
+	begin 
+		select * from "Employee";
+	end;
+$$ language plpgsql;
+
+select select_all_employees();
+
+create trigger return_all_employees
+	after insert on "Employee"
+	execute function select_all_employees()
+	
+insert into "Employee" ("EmployeeId", "LastName", "FirstName")
+values(11, 'test', 'test');*/
+
+
+/*
+ * 
+ * 7.0
+ * 
+ */
+	
+--7.1
+
+select c."FirstName",
+		c."LastName",
+		i."InvoiceId" 
+from "Customer" c 
+inner join "Invoice" i
+on i."CustomerId" = c."CustomerId";
+
+--7.2
+
+select c."CustomerId",
+		c."FirstName",
+		c."LastName",
+		i."InvoiceId",
+		i."Total" 
+from "Customer" c 
+full outer join "Invoice" i
+on i."CustomerId" = c."CustomerId";
+
+--7.3
+
+select "Name",
+		"Title"
+from "Artist" ar
+right join "Album" a
+on ar."ArtistId" = a."ArtistId";
+
+--7.4
+
+select * from "Album" a 
+cross join "Artist" ar
+order by ar."Name";
+
+--7.5
+
+select * from "Employee" e 
+inner join "Employee" e1
+on e."ReportsTo" = e1."ReportsTo" ;
+
+/*
+ * 
+ * 8.0
+ * 
+ */
+
+--8.1
+
+/*select * from "Employee" e 
+
+create index reports_to_index
+on "Employee"("Employee"."EmployeeId");
+*/
+
 
 
 		
