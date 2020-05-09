80daa98edbe59922fc2a4f81315f63a1f5858827
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-08 22:52:09 -0400

    Finished the testing for all the variables required in the first submit button.

--- Thomas_Hoang/DOM Assignment/working-with-the-dom.js
+++ Thomas_Hoang/DOM Assignment/working-with-the-dom.js
@@ -47,9 +47,58 @@ document.getElementById('planet').addEventListener('click', alienText);
 
 /*When the submit button is pressed, get the values from all of the input into a new row in the table below.  
 Make sure no input is empty, check that first and last name are at least two letters each. Validate for valid phone number 
-and email structure. This should continue to work for multiple entries and rows.
+and email structure. This should continue to work for multiple entries and rows.*/
+document.getElementById('form-sub').addEventListener('click', () => {
+    // let first = document.getElementById('firstname').value;
+    // if (first.length < 2) window.alert('Not a valid first name');
+    // let last = document.getElementById('lastname').value;
+    // if (last.length < 2) window.alert('Not a valid last name');
+    // let email = document.getElementById('email').value;
+    // let validEmail = /\S+@\S+\.\S+/;
+    // if (!email && !validEmail.test(email)) window.alert("Not a valid email.");
+    // let phone = document.getElementById('phone').value;
+    // let phoneValidator = /\d\d\d+\-\d\d\d+\-\d\d\d\d/
+    // if (!phone && !phoneValidator.test(phone)) window.alert("Not a valid phone number. EX:111-111-1111.");
+    // let birthday = document.getElementById('bday').value;
+    // let bdayValidator = /\d\d+\/+\d\d+\/\d\d\d\d/
+    // if (!birthday && !bdayValidator.test(birthday)) window.alert("Not a valid birthday. EX:01/01/1111.");
+    // let planets = document.getElementById('planet')
+    // let planet = planets.options(planets.selectedIndex).text;
+    // let gender = document.getElementsByName('gender').value;
+    // for (i=0;i<3;i++){
+    //     if (gender.checked)
+    //     gender = document.getElementsByName('gender')[i].value;
+    //     else window.alert('Please select a gender')
+    // }
+    // let color = document.getElementById('color').value;
+    let activity = [];
+    for (i=0;i<4;i++){
+    let selectedAnything = false;
+        if (document.getElementsByClassName('activity')[i].checked){
+            if (i = 0){
+                activity.push('hiking');
+                selectedAnything = true;
+            }else if (i = 1){
+                activity.push('stamp collection');
+                selectedAnything = true;
+            }else if (i = 2){
+                activity.push('birding');
+                selectedAnything = true;
+            }else if (i = 3){
+                activity.push('underwater basket weaver');
+                selectedAnything = true;
+            }
+        }else if (i = 3 && selectedAnything == false){
+            window.alert('Please select an activity even if they all suck.');
+        }
+    }
+    console.log(selectedAnything);
+    console.log(activity);
+});
 
-Create a function openDetails() which opens the details element. Invoke this function when the details’ summary is moused over. 
+
+
+/*Create a function openDetails() which opens the details element. Invoke this function when the details’ summary is moused over. 
 The details should be hidden when the mouse is removed from the summary.
 
 Create a function that concatenates the inner HTML of all of the span elements and prints the results to the console.
