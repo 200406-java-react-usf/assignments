7078a13342a35cba5bb7395adc8aa8480f1f969a
Jose Angeles <humblekgames@gmail.com>
2020-05-09 22:41:48 -0400

    finishes 5 and 6 but still having trouble with 4

--- DOM/working-with-the-dom.js
+++ DOM/working-with-the-dom.js
@@ -1,4 +1,5 @@
-// Make each link direct the user to its respective website 
+/* #1
+Make each link direct the user to its respective website */
 let firstLink = document.anchors[0];
 let secondLink = document.anchors[1];
 let thirdLink = document.anchors[2];
@@ -12,11 +13,13 @@ secondLink.href = 'https://twitter.com/';
 thirdLink.href = 'https://slack.com/';
 fourthLink.href = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript';
 
-// Disable the Pluto Planet of Residency option.
+/* #2
+Disable the Pluto Planet of Residency option. */
 let disablePluto = document.getElementById('planet');
 disablePluto.options[3].setAttribute('disabled', false);
 
-/*Define a function alienText() which shows the hidden element 
+/* #3
+Define a function alienText() which shows the hidden element 
 displaying an alien message. When any planet other than Earth is selected, 
 execute this function.*/
 let hiddenMsg = document.querySelector('br ~ p');
@@ -35,7 +38,8 @@ function alienText () {
 planet.addEventListener('click', alienText);
 console.log(mars.getAttribute('selected'));
 
-/* When the submit button is pressed, get the values 
+/* #4
+When the submit button is pressed, get the values 
 from all of the input into a new row in the table below.  
 Make sure no input is empty, check that first and last name 
 are at least two letters each. Validate for valid phone number 
@@ -43,13 +47,133 @@ and email structure. This should continue to work for
 multiple entries and rows. */
 let submitBtn = document.getElementById('form-sub');
 function newForm () {
-    let row = document.createComment('tr');
-    let fName = document.createComment('td');
-    let email = document.createElement('td');
-    let birthday = document.createElement('td');
-    let favColor = document.createElement('td');
-    let gender = document.createElement('td');
-    let activities = document.createElement('td');
+    // get the values of all the elements
+    let fn = document.getElementById('firstname').value;
+    let ln = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let bday = document.getElementById('bday').value;
+    let phone = document.getElementById('phone').value;
+    let favColor = document.getElementById('color').value;
 
+    // validate elements 
+    if (!fn || fn.length <= 2){
+        return;    
+    }
+    if (!ln || ln.length <= 2) {
+        return;
+    }
+    if (!email || !email.match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/')){
+        return;
+    };
+    if (!phone || phone.match('/^\d{10}$/')){
+        return;
+    };
+
+    if (!bday) {
+        return;
+    }
+
+    // create the values to save them into the HTML file
+    let createdRow = document.createComment('tr');
+    let createdFn = document.createComment('td');
+    let createdEmail = document.createElement('td');
+    let createdBday = document.createElement('td');
+    let createdPhone = document.createElement('td');
+    let createdColor = document.createElement('td');
+    let createdGender = document.createElement('td');
+    let createdActivitiesCell = document.createElement('td');
+    let createdActivitiesUL = document.createElement('ul');
+    
+    // append values into the newly created row
+    createdRow.appendChild(createdFn);
+    createdRow.appendChild(createdEmail);
+    createdRow.appendChild(createdPhone);
+    createdRow.appendChild(createdBday);
+    createdRow.appendChild(createdColor);
+    createdRow.appendChild(createdGender);
+    createdRow.appendChild(createdActivitiesCell);
+    createdActivitiesCell.append(createdActivitiesUL);
+
+    document.getElementsByTagName('tbody')[0].appendChild(createdRow); // appending the created row into the tbody
+
+    // Fill the html values
+    createdFn.innerHTML = fn; 
+    console.log(createdFn);
+    
+    createdEmail.innerHTML = email;
+    createdPhone.innerHTML = phone;
+    createdBday.innerHTML = bday;
+    createdColor.innerHTML = favColor; 
+    let checkedGender = document.getElementsByName('gender');
+    let checkedActivities = document.getElementsByClassName('activity');
+
+    let gender;
+    for (let i of checkedGender) {
+        if (i.checked){
+            gender = i.value;
+        }
+    }
+    if (!gender) return;
+    createdGender.appendChild(gender);
+
+    for (let i of checkedActivities) {
+        if (i .checked){
+            if(i.value === 'hiking'){
+                let li = document.createElement('li');
+                li.innerHTML = 'hiking';
+                createdActivitiesUL.appendChild(li);
+            }
+            if (i.value === 'stamp'){
+                let li = document.createElement('li');
+                li.innerHTML = 'stamp collecting';
+                createdActivitiesUL.appendChild(li);
+            }
+            if (i.value === 'birding'){
+                let li = document.createElement('li');
+                li.innerHTML = 'birding';
+                createdActivitiesUL.appendChild(li);
+            }
+            if (i.value === 'basket') {
+                let li = document.createElement('li');
+                li.innerHTML = 'underwater basket weaving';
+                createdActivitiesUL.appendChild(li);
+            }
+        }
+    }
+
+    document.getElementById('firstname').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('bday').value = '';
+    document.getElementById('color').value = '#000000';
+    document.getElementById('planet').selectedIndex = 0;
 }
 submitBtn.addEventListener('click', newForm);
+
+/* #5
+Create a function openDetails() which opens the details element. 
+Invoke this function when the details’ summary is moused over. 
+The details should be hidden when the mouse is removed from the summary.*/
+function openDetails(){
+    document.getElementsByTagName('details')[0].setAttribute('open', true);
+}
+document.getElementsByTagName('details')[0].addEventListener('mouseover', openDetails);
+
+function hideDetails(){
+    document.getElementsByTagName('details')[0].removeAttribute('open');
+}
+document.getElementsByTagName('details')[0].addEventListener('mouseout', hideDetails);
+
+/* #6
+Create a function that concatenates the inner HTML of all of the span elements 
+and prints the results to the console */
+function concatSpan () {
+    let span = document.getElementsByTagName('span');
+    let concatString = '';
+    for (let i of span) {
+        concatString += i.innerHTML;
+    }
+    // console.log(concatString);   
+}
+concatSpan();
