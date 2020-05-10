5c9813f7838d0302d1fca166d433ee988e4b1fea
KaneishaR <kaneisha.reese@gmail.com>
2020-05-10 14:03:22 -0500

    problem 4

--- working-with-the-dom/working-with-the-dom.js
+++ working-with-the-dom/working-with-the-dom.js
@@ -29,10 +29,6 @@ javadocs.setAttribute('target', '_blank');
 let pluto = document.querySelector('#planet > option:nth-child(4)');
 pluto.setAttribute('disabled', true);
 
-//Problem 2
-
-let pluto = document.querySelector('#planet > option:nth-child(4)');
-pluto.setAttribute('disabled', true);
 
 //Problem 3
 let earth = document.querySelector('#planet > option:nth-child(1)');
@@ -52,5 +48,133 @@ function alienText() {
 }
 planets.addEventListener('change', alienText);
 
+//Problem 4
+let submitBtn = document.getElementById('form-sub');
+let fname = document.getElementById('firstname');
+let lname = document.getElementById('lastname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let bday = document.getElementById('bday');
+let gender = document.querySelectorAll('input[name="gender"]');
+let color = document.getElementById('color');
+let activities = document.querySelectorAll('input[class="activity"]')
+//make sure all inputs not empty
+fname.setAttribute('required', true);
+lname.setAttribute('required', true);
+email.setAttribute('required', true);
+phone.setAttribute('required', true);
+bday.setAttribute('required', true);
+color.setAttribute('required', true);
+
+//format inputs
+fname.setAttribute('minlength', 2);
+lname.setAttribute('minlength', 2);
+email.setAttribute('pattern','[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+phone.setAttribute('pattern','[0-9]{3}-[0-9]{3}-[0-9]{4}');
+
+//on submit click add row to table
+submitBtn.addEventListener('click', addNewRow);
+
+function addNewRow(){
+    let form = document.querySelector('form.form-group');
+    
+    //validation of buttons
+    let invalidradio, invalidcheck;
+    for(const radio of gender) {
+        if(radio.checked){
+            invalidradio = true;
+            break;
+        }
+        else{
+            invalidradio = false;
+        }
+    }
+
+    for(const check of activities) {
+        if(check.checked){
+            invalidcheck = true;
+        }
+        else{
+            invalidcheck = false;
+        }
+
+        //check validity 
+        if(fname.validity.valid && lname.validity.valid && 
+        email.validity.valid && bday.validity.valid && invalidradio
+        && invalidcheck){
+            //create new cells
+            let namecell = document.createElement('td');
+            let emailcell = document.createElement('td');
+            let phonecell = document.createElement('td');
+            let bdaycell = document.createElement('td');
+            let colorcell = document.createElement('td');
+            let gendercell = document.createElement('td');
+            let activitescell = document.createElement('td');
+            let newEntry = document.createElement('tr');
+
+            let unorder = document.createElement('ul');
+            activitescell.appendChild(unorder);
+            
+            //add cells to new row
+            newEntry.appendChild(namecell);
+            newEntry.appendChild(emailcell);
+            newEntry.appendChild(phonecell);
+            newEntry.appendChild(bdaycell);
+            newEntry.appendChild(colorcell);
+            newEntry.appendChild(gendercell);
+            newEntry.appendChild(activitescell);
+            
+            //add new row to the table
+            let table = document.querySelector('table.table');
+            table.appendChild(newEntry);
+            table.setAttribute('scope','col');
+            
+            //assign values to the cells
+            for(const radio of gender) {
+                if(radio.checked){
+                    gendercell.innerText = radio.value;
+                    break;
+                }
+            }
+            for(const check of activities) {
+                if(check.checked){
+                    let listItem = document.createElement('li');
+                    listItem.innerText = check.value;
+                    unorder.appendChild(listItem);
+                }
+            }
+            namecell.innerText = `${fname.value} and ${lname.value}`;
+            emailcell.innerText = email.value;
+            phonecell.innerText = phone.value;
+            formatdate = new Date(bday.value);
+            bdaycell.innerText = new Date(formatdate.getTime() - formatdate.getTimezoneOffset() * -60000 ).toDateString();
+            colorcell.innerText = color.value;
+    
+            //clear cells for next entry
+            fname.value = '';
+            lname.value = '';
+            email.value = '';
+            phone.value = '';
+            bday.value = '';
+            color.value = '#000000';
+    
+    
+        } 
+}
+//Problem 5
 
 
+//Problem 6
+
+
+//Problem 7
+
+
+//Problem 8
+
+
+//Problem 9
+
+
+//Problem 10
+}
