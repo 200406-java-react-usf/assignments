af52d1abd0cad0f9c5f10e9a242bf1750ea0ae50
Jose Angeles <humblekgames@gmail.com>
2020-05-10 00:41:12 -0400

    Finished all problems except for number 4, still bugged

--- DOM/working-with-the-dom.js
+++ DOM/working-with-the-dom.js
@@ -1,5 +1,7 @@
-/* #1
-Make each link direct the user to its respective website */
+/* 
+    #1
+    Make each link direct the user to its respective website 
+*/
 let firstLink = document.anchors[0];
 let secondLink = document.anchors[1];
 let thirdLink = document.anchors[2];
@@ -13,15 +15,19 @@ secondLink.href = 'https://twitter.com/';
 thirdLink.href = 'https://slack.com/';
 fourthLink.href = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript';
 
-/* #2
-Disable the Pluto Planet of Residency option. */
+/* 
+    #2
+    Disable the Pluto Planet of Residency option. 
+*/
 let disablePluto = document.getElementById('planet');
 disablePluto.options[3].setAttribute('disabled', false);
 
-/* #3
-Define a function alienText() which shows the hidden element 
-displaying an alien message. When any planet other than Earth is selected, 
-execute this function.*/
+/* 
+    #3
+    Define a function alienText() which shows the hidden element 
+    displaying an alien message. When any planet other than Earth is selected, 
+    execute this function.
+*/
 let hiddenMsg = document.querySelector('br ~ p');
 let planet = document.getElementById('planet');
 let earth = document.querySelector('#planet > option:nth-child(1)');
@@ -36,15 +42,16 @@ function alienText () {
         }   
 }
 planet.addEventListener('click', alienText);
-console.log(mars.getAttribute('selected'));
 
-/* #4
-When the submit button is pressed, get the values 
-from all of the input into a new row in the table below.  
-Make sure no input is empty, check that first and last name 
-are at least two letters each. Validate for valid phone number 
-and email structure. This should continue to work for 
-multiple entries and rows. */
+/* 
+    #4
+    When the submit button is pressed, get the values 
+    from all of the input into a new row in the table below.  
+    Make sure no input is empty, check that first and last name 
+    are at least two letters each. Validate for valid phone number 
+    and email structure. This should continue to work for 
+    multiple entries and rows. 
+*/
 let submitBtn = document.getElementById('form-sub');
 function newForm () {
     // get the values of all the elements
@@ -62,10 +69,12 @@ function newForm () {
     if (!ln || ln.length <= 2) {
         return;
     }
-    if (!email || !email.match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/')){
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!email || !emailRegex.test(email)){
         return;
     };
-    if (!phone || phone.match('/^\d{10}$/')){
+    let phoneRegex = /^\d{10}$/;
+    if (!phone || !phoneRegex.test(phone)){
         return;
     };
 
@@ -82,7 +91,9 @@ function newForm () {
     let createdColor = document.createElement('td');
     let createdGender = document.createElement('td');
     let createdActivitiesCell = document.createElement('td');
-    let createdActivitiesUL = document.createElement('ul');
+    let createdActivitiesUL = document.createElement('ul');  
+
+    document.getElementsByTagName('tbody')[0].appendChild(createdRow); // appending the created row into the tbody
     
     // append values into the newly created row
     createdRow.appendChild(createdFn);
@@ -94,12 +105,8 @@ function newForm () {
     createdRow.appendChild(createdActivitiesCell);
     createdActivitiesCell.append(createdActivitiesUL);
 
-    document.getElementsByTagName('tbody')[0].appendChild(createdRow); // appending the created row into the tbody
-
     // Fill the html values
     createdFn.innerHTML = fn; 
-    console.log(createdFn);
-    
     createdEmail.innerHTML = email;
     createdPhone.innerHTML = phone;
     createdBday.innerHTML = bday;
@@ -151,10 +158,12 @@ function newForm () {
 }
 submitBtn.addEventListener('click', newForm);
 
-/* #5
-Create a function openDetails() which opens the details element. 
-Invoke this function when the details’ summary is moused over. 
-The details should be hidden when the mouse is removed from the summary.*/
+/* 
+    #5
+    Create a function openDetails() which opens the details element. 
+    Invoke this function when the details’ summary is moused over. 
+    The details should be hidden when the mouse is removed from the summary.
+*/
 function openDetails(){
     document.getElementsByTagName('details')[0].setAttribute('open', true);
 }
@@ -165,9 +174,11 @@ function hideDetails(){
 }
 document.getElementsByTagName('details')[0].addEventListener('mouseout', hideDetails);
 
-/* #6
-Create a function that concatenates the inner HTML of all of the span elements 
-and prints the results to the console */
+/* 
+    #6
+    Create a function that concatenates the inner HTML of all of the span elements 
+    and prints the results to the console 
+*/
 function concatSpan () {
     let span = document.getElementsByTagName('span');
     let concatString = '';
@@ -177,3 +188,100 @@ function concatSpan () {
     // console.log(concatString);   
 }
 concatSpan();
+
+/* 
+    #7
+    Create a function that displays the current time on earth in the span with 
+    id “earth_time”. Invoke this function when “Earth time” button is clicked. 
+*/
+let earthBtn = document.getElementById('earth_time_check');
+function displayEarth() {
+    let timePlacer = document.getElementById('earth_time');
+    let newSpan = document.createElement('span');
+    let today = new Date();
+    let currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    timePlacer.appendChild(newSpan);
+    newSpan.innerHTML = currentTime;
+    document.getElementById('earth_time').value = '';
+}
+earthBtn.addEventListener('click', displayEarth);
+document.getElementsByTagName
+/* 
+    #8 
+    Three seconds after a user clicks on the “Intergalactic Directory” heading, 
+    the background color should change to a random color. Make sure this color is 
+    never black so we can still read our black text! (there are other dark colors 
+    it could change to where we also couldn’t see the text but it’s enough to just 
+    accomodate for a black background)
+*/
+let intergalacticDir = document.getElementsByTagName('h1')[0];
+function headingColorChange () {
+    setTimeout (() => {
+        let R = Math.floor(Math.random() * 256);
+        let G = Math.floor(Math.random() * 256);
+        let B = Math.floor(Math.random() * 256);
+
+        let randColor = 'rgb(' + R + ',' + G + ',' + B + ')';
+
+        if(randColor !== 'rgb(0,0,0)'){
+            document.getElementsByTagName('h1')[0].style.backgroundColor = randColor;
+        }
+    }, 3000)
+}
+intergalacticDir.addEventListener('click', headingColorChange);
+
+/* 
+    #9
+    When inputs with id n1 and n2 have valid numerical input, 
+    perform the operation specified in the select. Display the result 
+    in the element with id result. 
+*/
+function calculator(){
+
+    let n1 = +document.getElementById('n1').value; 
+    let n2 = +document.getElementById('n2').value; 
+    let operatorDropdown = document.getElementById('operation');
+    let typeOfOperation = operatorDropdown.options[operatorDropdown.selectedIndex].text;
+    let result = document.getElementById('result');
+    
+    if (!n1 || !n2) return;
+
+    if (typeOfOperation === 'Add'){
+        result.innerHTML = n1 + n2;
+    }
+
+    if (typeOfOperation === 'Subtract'){
+        result.innerHTML = n1 - n2;
+    }
+
+    if (typeOfOperation === 'Multiply'){
+        result.innerHTML = n1 * n2;
+    }
+
+    if (typeOfOperation === 'Divide'){
+        result.innerHTML = n1 / n2;
+    }
+
+}
+
+document.getElementById('n1').addEventListener('blur', calculator);
+document.getElementById('n2').addEventListener('blur', calculator);
+
+/* 
+    #10
+    Define function walkTheDom(node, func)
+    This function should traverse every node in the DOM. 
+    Use recursion. On each node, calle func(node).
+*/
+
+function walkTheDom(node, func){
+    node = node.firstChild; // initializing the first node   
+    func(node);
+    // while a node still exists in the file
+    while(node){
+        walkTheDom(node,func); // call the walkTheDom 
+        node = node.nextSibling; // and then move to the next sibling
+    }
+}
+walkTheDom(document.getElementsByTagName('body')[0], node => console.log(node));
